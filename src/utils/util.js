c8b38dde7fc18e652c6cf8419312a36df4490a69
penguin0731 <276087919@qq.com>
2021-03-15 22:32:23 +0800

    常用的工具函数

---
+++ src/utils/util.js
@@ -0,0 +1,21 @@
+/**
+ * 补0函数
+ * @param {*} s 数字
+ * @returns 
+ */
+export function addZero(s) {
+  return s < 10 ? `0${s}` : s;
+}
+
+
+/**
+ * 格式化歌曲时间
+ * @param {*} duration 歌曲时间(ms)
+ * @returns 
+ */
+export function format(duration) {
+  duration /= 1000;
+  let minutes = Math.floor(duration / 60);
+  let seconds = Math.floor(duration % 60);
+  return `${addZero(minutes)}:${addZero(seconds)}`;
+}

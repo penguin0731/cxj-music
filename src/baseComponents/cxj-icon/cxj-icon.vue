9fcfff3129e9d1b5b60c81ca1b5bb4f4b4ea7638
penguin0731 <276087919@qq.com>
2021-03-23 23:19:23 +0800

    icon组件，播放列表dom结构

---
+++ src/baseComponents/cxj-icon/cxj-icon.vue
@@ -0,0 +1,33 @@
+<template>
+  <i
+    :class="{
+      icon_sprite: type == 'icon',
+      player_sprite: type == 'player',
+    }"
+  ></i>
+</template>
+
+<script>
+export default {
+  props: {
+    type: {
+      type: String,
+      default: "icon",
+    },
+    icon: {
+      type: String,
+    },
+  },
+  setup(props) {
+    return {
+      ...props,
+    };
+  },
+};
+</script>
+
+<style lang="less" scoped>
+i {
+  display: inline-block;
+}
+</style>
--- src/components/playlist/playlist.vue
+++ src/components/playlist/playlist.vue
@@ -1,15 +1,71 @@
 <template>
   <div class="playlist_box">
-
+    <div class="playlist_top">
+      <h4>播放列表</h4>
+      <div class="h-r">
+        <div class="playlist_btn"><cxj-icon class="icon_clear" />清空列表</div>
+        <cxj-icon class="icon_close" />
+      </div>
+    </div>
+    <div class="playlist_con">
+      
+    </div>
   </div>
 </template>
 
 <script>
-export default {
+import cxjIcon from "@/baseComponents/cxj-icon/cxj-icon.vue";
 
-}
+export default {
+  components: { cxjIcon },
+};
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.playlist_box {
+  width: 540px;
+  height: 280px;
+  background-color: rgba(25, 25, 25, 1);
+  color: #bdbdbe;
+  padding: 25px 0 23px;
+  border-top-left-radius: 6px;
+  border-top-right-radius: 6px;
+}
+.playlist_top {
+  display: flex;
+  justify-content: space-between;
+  padding: 0 25px;
+  .h-r {
+    display: flex;
+  }
+  .playlist_btn {
+    display: flex;
+    align-items: center;
+    margin-right: 20px;
+    cursor: pointer;
+    &:hover {
+      color: #fff;
+      .icon_clear {
+        opacity: 1;
+      }
+    }
+  }
+}
+.icon_clear {
+  opacity: 0.8;
+  width: 17px;
+  height: 17px;
+  background-position: -40px -300px;
+  margin-right: 5px;
+}
+.icon_close {
+  opacity: 0.8;
+  width: 20px;
+  height: 20px;
+  background-position: -37px -197px;
+  cursor: pointer;
+  &:hover {
+    opacity: 1;
+  }
+}
 </style>

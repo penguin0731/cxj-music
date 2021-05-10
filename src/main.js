6273e054e5308b801a067515ba278c10bd571f52
penguin0731 <276087919@qq.com>
2021-05-10 15:06:53 +0800

    新增nProgress进度条库

--- src/main.js
+++ src/main.js
@@ -2,7 +2,23 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+import '@/assets/style/myProgress.css'
 import '@/assets/style/common.css'
+
+
+
+NProgress.configure({ showSpinner: false });
+window.NProgress = NProgress
+router.beforeEach((to, from, next) => {
+  NProgress.start();
+  next();
+})
+
+router.afterEach((to, from) => {
+  NProgress.done()
+})
 
 const app = createApp(App);
 app.use(router)

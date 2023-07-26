8b94974a05da78d6ed87980a56118b890b8a691d
penguin0731 <276087919@qq.com>
2023-07-26 15:35:24 +0800

    项目重构

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+import setupExtend from 'vite-plugin-vue-setup-extend';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), setupExtend()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
